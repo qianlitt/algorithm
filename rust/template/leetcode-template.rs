448d04d752d85a231d13c27583f4414ea269905f
qianlitt <zvdiek@gmail.com>
2025-12-18 00:48:32 +0800

    init: 初始化Rust算法题目录

---
+++ rust/template/leetcode-template.rs
@@ -0,0 +1,15 @@
+struct Solution;
+
+impl Solution {}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {}
+}
